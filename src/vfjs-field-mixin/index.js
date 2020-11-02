7c7b7f09a8e36d232b44ef12a8a1e1157a6c4f82
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2020-11-03 00:16:12 +0100

    Remove unused data property

--- src/vfjs-field-mixin/index.js
+++ src/vfjs-field-mixin/index.js
@@ -4,11 +4,6 @@ import methods from './methods';
 import props from './props';
 
 const vfjsFieldMixin = {
-  data() {
-    return {
-      vfjsSetFieldTimeout: null,
-    };
-  },
   computed,
   props,
   emits,
