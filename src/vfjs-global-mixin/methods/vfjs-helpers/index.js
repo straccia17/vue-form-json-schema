7273651666015478b6318332c5bb3a71e6c40b02
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2020-11-03 21:09:43 +0100

    Add internalModel/vfjsFieldInternalModel option

--- src/vfjs-global-mixin/methods/vfjs-helpers/index.js
+++ src/vfjs-global-mixin/methods/vfjs-helpers/index.js
@@ -16,6 +16,7 @@ const vfjsHelpers = {
       required: vfjsFieldRequired = false,
       valueProp: vfjsFieldValueProp = this.vfjsOptions.valueProp,
       valueModel: vfjsFieldValueModelKey,
+      internalModel: vfjsFieldInternalModel = false,
     } = vfjsFieldUiSchema;
 
     const vfjsFieldSchema = this.getVfjsFieldSchema(vfjsFieldModelKey) || {};
@@ -56,6 +57,7 @@ const vfjsHelpers = {
       vfjsFieldErrors,
       vfjsFieldEventProp,
       vfjsFieldId,
+      vfjsFieldInternalModel,
       vfjsFieldModel,
       vfjsFieldModelKey,
       vfjsFieldOptions,
