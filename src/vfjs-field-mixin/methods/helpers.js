941107e5ede3eeebd3be07431f0aaf6a4b38d49f
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2020-11-23 22:50:05 +0100

    Basic option to prefix 'on' to events to handle

--- src/vfjs-field-mixin/methods/helpers.js
+++ src/vfjs-field-mixin/methods/helpers.js
@@ -94,13 +94,16 @@ const helpers = {
     };
   },
   vfjsFieldHelperFormatEventsReducer(events = {}) {
-    return Object.keys(events).reduce(
-      (formattedEvents, key) => ({
+    return Object.keys(events).reduce((formattedEvents, key) => {
+      const rest = key.substring(1);
+      const firstLetter = key[0].toUpperCase();
+      const property = `on${firstLetter}${rest}`;
+
+      return {
         ...formattedEvents,
-        [key]: this.vfjsFieldHelperEventHandler(key, events[key]),
-      }),
-      {},
-    );
+        [property]: this.vfjsFieldHelperEventHandler(key, events[key]),
+      };
+    }, {});
   },
   vfjsFieldHelperComponentMatchesComponentProperties(componentProperties) {
     return componentProperties.some((componentProperty) => {
